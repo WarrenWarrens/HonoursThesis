181c29621018ae576f4342ea556f13c09e0c1260
Ryan Warrener <ryan.warrener@ontariotechu.net>
2025-10-02 12:03:52 -0400

    Basic Extension functionality that allows a screen preview to be shown and the selection of brave browser, specific tab or entire screen

---
+++ Test2/background.js
@@ -0,0 +1,5 @@
+// background.js
+// Currently not used, but ready for messaging or future logic.
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Screen Share Demo extension installed.");
+});
