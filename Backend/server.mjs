8f0ac5348f1bf6e36a4312c45ca8c91a7c76c4b9
Ryan Warrener <ryan.warrener@ontariotechu.net>
2025-11-05 13:48:08 -0500

    Adjusted file structure to be more simple and divided between front and back end

--- Backend/server.mjs
+++ Backend/server.mjs
@@ -2,6 +2,7 @@
 import express from "express";
 import http from "http";
 import { WebSocketServer } from "ws";
+
 import path from "path";
 import { fileURLToPath } from "url";
 import { randomUUID } from "crypto";
